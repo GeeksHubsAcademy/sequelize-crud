3db9810aa76ec9d8f3bba93a5336e66ca6320c87
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-06-11 14:01:34 +0200

    added DirectorId field to the Movie model

--- api/migrations/20200610095648-create-movie.js
+++ api/migrations/20200610095648-create-movie.js
@@ -1,33 +1,36 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Movies', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      poster_path: {
-        type: Sequelize.STRING
-      },
-      overview: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Movies');
-  }
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Movies', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            title: {
+                type: Sequelize.STRING
+            },
+            poster_path: {
+                type: Sequelize.STRING
+            },
+            overview: {
+                type: Sequelize.TEXT
+            },
+            DirectorId: {
+                type: Sequelize.INTEGER
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Movies');
+    }
 };
--- api/models/movie.js
+++ api/models/movie.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Movie = sequelize.define('Movie', {
-    title: DataTypes.STRING,
-    poster_path: DataTypes.STRING,
-    overview: DataTypes.TEXT
-  }, {});
-  Movie.associate = function(models) {
-    // associations can be defined here
-  };
-  return Movie;
+    const Movie = sequelize.define('Movie', {
+        title: DataTypes.STRING,
+        poster_path: DataTypes.STRING,
+        overview: DataTypes.TEXT,
+        DirectorId: DataTypes.INTEGER
+    }, {});
+    Movie.associate = function(models) {
+        // associations can be defined here
+    };
+    return Movie;
 };
