79e3004969485b750d7445473ca32450741135e2
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-06-11 13:03:15 +0200

    added movies routes

--- api/index.js
+++ api/index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const { Movie } = require('./models');
 const app = express();
+const moviesRouter = require('./routes/movies')
 const PORT = 3000
-app.use(express.json()); //parseamos el body de la petición a json
-app.get('/movies', (req, res) => {
-    Movie.findAll().then(movies => res.send(movies))
-})
-app.post('/movies', (req, res) => {
-    Movie.create(req.body) //insertamos una fila en la tabla movies //INSERT INTO movies.movies () values(..,..,.)
-        .then(movie => res.status(201).send(movie)) //si se inserta correctamente entrará a ejecutar el callback del then
-        .catch(error => {
-            console.error(error)
-            res.status(500).send({ message: 'There was a problem trying to add the movie' });
-        }) //si hay un problema pues entrará a ejecutar el callback del catch
-        // .finally(() => console.log('esto es el finally')); //el finally se ejecuta independientemente del resultado
+app.use(express.json()); //parseamos el body de la petición a json sino req.body es undefined
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next(); //imprescindible next para que ejecute la siguiente función
 });
+app.use('/movies', moviesRouter);
 app.listen(PORT, () => console.log('server running on port ' + PORT))
--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,20 @@
-fetch('http://localhost:3000/movies') //Hace un GET a la url especificada
-    .then(res => res.json()) //parsea la respuesta que viene en json
-    .then(movies => console.log(movies));
+const moviesDiv = document.querySelector('.peliculas');
+const getMovies = async() => {
+        try {
+            const res = await fetch('http://localhost:3000/movies') //Hace un GET a la url especificada
+            const movies = await res.json(); //parsea la respuesta que viene en json
+            movies.forEach(movie => {
+                moviesDiv.innerHTML += `
+                    <div class="pelicula">
+                    <h2>${movie.title}</h2>
+                    <img src="${movie.poster_path}"/>
+                    <p>${movie.overview}</p>
+                    </div>
+                    `
+            });
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    // getMovies().catch(console.error);
+getMovies();
